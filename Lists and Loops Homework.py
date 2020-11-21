1aac4c1bdc96f6fe8cde541e16f9216030becba8
MoDux554 <73943731+MoDux554@users.noreply.github.com>
2020-11-21 16:17:00 +0000

    Add files via upload

---
+++ Lists and Loops Homework.py
@@ -0,0 +1,62 @@
+
+# x =  ['mix', 'xyz', 'apple', 'xanadu', 'rovio']
+
+
+# list_1 = [] #for the words above minus xyz and xanadu
+# list_2 = [] #for xyz and xanadu
+#
+# # for letters in x:
+# #     list_1.append(letters)
+# #     print(letters)
+
+
+
+
+
+#lll
+
+
+# for letters in x:
+#     if letters[0][0] == "x":
+#         print(letters)
+
+
+#Exercise 2
+
+# words = ["xxx", "aaa", "r5t6yy", "g", 'wow']
+#
+#
+# for i in words:
+#    if len(i) >= 2:
+#        if i[0] == i[2]:
+#            print(i)
+
+
+
+
+#Exercise 3
+
+# numbers = [2,3,5,7,66,89,134]
+#
+# usrInput = int(input("enter in a number"))
+#
+#
+# for num in numbers:
+#     if num < usrInput:
+#         print(num)
+
+
+#Bonus Exercise
+
+
+# List = ['*']
+#
+# count = 0
+#
+# List_2 = []
+#
+# while count <= 10:
+#     for l in List:
+#         count+=1
+#         List_2.append(l)
+#         print(List_2)
